deca9c132e89b00b886a1fa211aedc823a1730bd
Ben Firshman <ben@firshman.co.uk>
2010-06-12 00:49:16 +0100

    Removed unnecessary inheritance util

--- js.2/utils.js
+++ js.2/utils.js
@@ -4,17 +4,5 @@ JSNES.Utils = {
         for (var i=0; i<length; ++i) {
             dest[destPos+i] = src[srcPos+i];
         }
-    },
-    
-    // http://www.sitepoint.com/blogs/2006/01/17/javascript-inheritance/
-    copyPrototype: function(descendant, parent) {
-        var sConstructor = parent.toString();
-        var aMatch = sConstructor.match( /\s*function (.*)\(/ );
-        if ( aMatch != null ) { descendant.prototype[aMatch[1]] = parent; }
-        for (var m in parent.prototype) {
-            if (typeof descendant.prototype[m] == 'undefined') {
-                descendant.prototype[m] = parent.prototype[m];
-            }
-        }
     }
 }
