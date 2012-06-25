48a6ba83406612f705ca04f265b9e1d1395c1cd0
Guillaume Marty <edo999@gmail.com>
2012-06-25 19:12:51 +0200

    Source cleaning

--- source/utils.js
+++ source/utils.js
@@ -17,42 +17,42 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 JSNES.Utils = {
-    copyArrayElements: function(src, srcPos, dest, destPos, length) {
-        for (var i = 0; i < length; ++i) {
-            dest[destPos + i] = src[srcPos + i];
-        }
-    },
-    
-    copyArray: function(src) {
-        var dest = new Array(src.length);
-        for (var i = 0; i < src.length; i++) {
-            dest[i] = src[i];
-        }
-        return dest;
-    },
-    
-    fromJSON: function(obj, state) {
-        for (var i in obj.JSON_PROPERTIES) {
-            if (!obj.JSON_PROPERTIES.hasOwnProperty(i)) {
-                continue;
-            }
-            obj[i] = state[obj.JSON_PROPERTIES[i]];
-        }
-    },
-    
-    toJSON: function(obj) {
-        var state = {}, i;
-        for (i in obj.JSON_PROPERTIES) {
-            if (!obj.JSON_PROPERTIES.hasOwnProperty(i)) {
-                continue;
-            }
-            state[obj.JSON_PROPERTIES[i]] = obj[i];
-        }
-        return state;
-    },
-    
-    isIE: function() {
-        return (/msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent));
+  copyArrayElements: function(src, srcPos, dest, destPos, length) {
+    for (var i = 0; i < length; ++i) {
+      dest[destPos + i] = src[srcPos + i];
     }
+  },
+
+  copyArray: function(src) {
+    var dest = new Array(src.length);
+    for (var i = 0; i < src.length; i++) {
+      dest[i] = src[i];
+    }
+    return dest;
+  },
+
+  fromJSON: function(obj, state) {
+    for (var i in obj.JSON_PROPERTIES) {
+      if (!obj.JSON_PROPERTIES.hasOwnProperty(i)) {
+        continue;
+      }
+      obj[i] = state[obj.JSON_PROPERTIES[i]];
+    }
+  },
+
+  toJSON: function(obj) {
+    var state = {}, i;
+    for (i in obj.JSON_PROPERTIES) {
+      if (!obj.JSON_PROPERTIES.hasOwnProperty(i)) {
+        continue;
+      }
+      state[obj.JSON_PROPERTIES[i]] = obj[i];
+    }
+    return state;
+  },
+
+  isIE: function() {
+    return (/msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent));
+  }
 };
 
