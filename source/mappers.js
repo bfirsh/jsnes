309c6404d9e1db16c24e1fef833a86b45b516e79
Webinternals <i@webinternals.org>
2012-09-28 17:15:10 +0400

    Added BNROM Mapper (iNES Mapper 034)
    http://wiki.nesdev.com/w/index.php/INES_Mapper_034
    Example games: Mashou, Darkseed, Impossible Mission 2

--- source/mappers.js
+++ source/mappers.js
@@ -1179,3 +1179,26 @@ JSNES.Mappers[11].prototype.write = function(address, value) {
         }
     }
 };
+
+/**
+ * Mapper 034 (BNROM, NINA-01)
+ * 
+ * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_034
+ * @example Darkseed, Mashou, Mission Impossible 2
+ * @constructor
+ */
+JSNES.Mappers[34] = function(nes) {
+    this.nes = nes;
+    console.log('mapper 034 used');
+};
+
+JSNES.Mappers[34].prototype = new JSNES.Mappers[0]();
+
+JSNES.Mappers[34].prototype.write = function(address, value) {
+    if (address < 0x8000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+        return;
+    } else {
+        this.load32kRomBank(value, 0x8000);
+    }
+}
