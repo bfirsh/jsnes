5cecd882f16743cef53ac3a0a5d1e5714b07b1f1
Webinternals <i@webinternals.org>
2012-09-28 16:51:03 +0400

    Added Color Dreams Mapper (iNES Mapper 011)
    http://wiki.nesdev.com/w/index.php/Color_Dreams
    Example games: Crystal Mines, Metal Fighter

--- source/mappers.js
+++ source/mappers.js
@@ -1143,3 +1143,39 @@ JSNES.Mappers[4].prototype.fromJSON = function(s) {
     this.irqEnable = s.irqEnable;
     this.prgAddressChanged = s.prgAddressChanged;
 };
+
+
+/**
+ * Mapper 011 (Color Dreams)
+ * 
+ * @description http://wiki.nesdev.com/w/index.php/Color_Dreams
+ * @example Crystal Mines, Metal Fighter
+ * @constructor
+ */
+JSNES.Mappers[11] = function(nes) {
+    this.nes = nes;    
+};
+
+JSNES.Mappers[11].prototype = new JSNES.Mappers[0]();
+
+JSNES.Mappers[11].prototype.write = function(address, value) {
+    if (address < 0x8000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+        return;
+    } else {
+        // Swap in the given PRG-ROM bank:
+        var prgbank1 = ((value & 0xF) * 2) % this.nes.rom.romCount;
+        var prgbank2 = ((value & 0xF) * 2 + 1) % this.nes.rom.romCount;
+
+        this.loadRomBank(prgbank1, 0x8000);
+        this.loadRomBank(prgbank2, 0xC000);
+
+
+        if (this.nes.rom.vromCount > 0) {
+            // Swap in the given VROM bank at 0x0000:
+            var bank = ((value >> 4) * 2) % (this.nes.rom.vromCount);
+            this.loadVromBank(bank, 0x0000);
+            this.loadVromBank(bank + 1, 0x1000);
+        }
+    }
+};
