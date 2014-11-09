116035bf505cc8f8908ef2370d53b366a4d7106f
Valery <valeriy.blagodarniy@gmail.com>
2014-11-09 22:56:36 +0300

    New mappers
    From https://github.com/bfirsh/jsnes/pull/18

--- source/mappers.js
+++ source/mappers.js
@@ -1126,3 +1126,267 @@ JSNES.Mappers[4].prototype.fromJSON = function(s) {
     this.irqEnable = s.irqEnable;
     this.prgAddressChanged = s.prgAddressChanged;
 };
+
+
+/**
+ * Mapper005 (MMC5,ExROM)
+ *
+ * @example Castlevania 3, Just Breed, Uncharted Waters, Romance of the 3 Kingdoms 2, Laser Invasion, Metal Slader Glory, Uchuu Keibitai SDF, Shin 4 Nin Uchi Mahjong - Yakuman Tengoku
+ * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_005
+ * @constructor
+ */
+JSNES.Mappers[5] = function(nes) {
+    this.nes = nes;
+};
+
+JSNES.Mappers[5].prototype = new JSNES.Mappers[0]();
+
+JSNES.Mappers[5].prototype.write = function(address, value) {
+    // Writes to addresses other than MMC registers are handled by NoMapper.
+    if (address < 0x8000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+    } else {
+        this.load8kVromBank(value, 0x0000);
+    }
+};
+
+JSNES.Mappers[5].prototype.write = function(address, value) {
+    // Writes to addresses other than MMC registers are handled by NoMapper.
+    if (address < 0x5000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+        return;
+    }
+
+    switch(address) {
+        case 0x5100: this.prg_size = value & 3; break;
+        case 0x5101: this.chr_size = value & 3; break;
+        case 0x5102: this.sram_we_a = value & 3; break;
+        case 0x5103: this.sram_we_b = value & 3; break;
+        case 0x5104: this.graphic_mode = value & 3; break;
+        case 0x5105:
+            this.nametable_mode = value;
+            this.nametable_type[0] = value & 3;
+            this.load1kVromBank(value & 3, 0x2000);
+            value >>= 2;
+            this.nametable_type[1] = value & 3;
+            this.load1kVromBank(value & 3, 0x2400);
+            value >>= 2;
+            this.nametable_type[2] = value & 3;
+            this.load1kVromBank(value & 3, 0x2800);
+            value >>= 2;
+            this.nametable_type[3] = value & 3;
+            this.load1kVromBank(value & 3, 0x2C00);
+            break;
+        case 0x5106: this.fill_chr = value; break;
+        case 0x5107: this.fill_pal = value & 3; break;
+        case 0x5113: this.SetBank_SRAM(3, value & 3); break;
+        case 0x5114:
+        case 0x5115:
+        case 0x5116:
+        case 0x5117:
+            this.SetBank_CPU(address, value);
+            break;
+        case 0x5120:
+        case 0x5121:
+        case 0x5122:
+        case 0x5123:
+        case 0x5124:
+        case 0x5125:
+        case 0x5126:
+        case 0x5127:
+            this.chr_mode = 0;
+            this.chr_page[0][address&7] = value;
+            this.SetBank_PPU();
+            break;
+        case 0x5128:
+        case 0x5129:
+        case 0x512A:
+        case 0x512B:
+            this.chr_mode = 1;
+            this.chr_page[1][(address&3)+0] = value;
+            this.chr_page[1][(address&3)+4] = value;
+            this.SetBank_PPU();
+            break;
+        case 0x5200: this.split_control = value; break;
+        case 0x5201: this.split_scroll = value; break;
+        case 0x5202: this.split_page = value & 0x3F; break;
+        case 0x5203:
+            this.irq_line = value;
+            this.nes.cpu.ClearIRQ();
+            break;
+        case 0x5204:
+            this.irq_enable = value;
+            this.nes.cpu.ClearIRQ();
+            break;
+        case 0x5205: this.mult_a = value; break;
+        case 0x5206: this.mult_b = value; break;
+        default:
+            if(address >= 0x5000 && address <= 0x5015) {
+                this.nes.papu.exWrite(address, value);
+            } else if(address >= 0x5C00 && address <= 0x5FFF) {
+                if(this.graphic_mode == 2) { 			// ExRAM
+                    // vram write
+                } else if(this.graphic_mode != 3) {		// Split,ExGraphic
+                    if(this.irq_status & 0x40) {
+                        // vram write
+                    } else {
+                        // vram write
+                    }
+                }
+            } else if(address >= 0x6000 && address <= 0x7FFF) {
+                if(this.sram_we_a == 2 && this.sram_we_b == 1) {
+                    // additional ram write
+                }
+            }
+            break;
+    }
+};
+
+JSNES.Mappers[5].prototype.loadROM = function(rom) {
+    if (!this.nes.rom.valid) {
+        alert("UNROM: Invalid ROM! Unable to load.");
+        return;
+    }
+
+    // Load PRG-ROM:
+    this.load8kRomBank(this.nes.rom.romCount * 2 - 1, 0x8000);
+    this.load8kRomBank(this.nes.rom.romCount * 2 - 1, 0xA000);
+    this.load8kRomBank(this.nes.rom.romCount * 2 - 1, 0xC000);
+    this.load8kRomBank(this.nes.rom.romCount * 2 - 1, 0xE000);
+
+    // Load CHR-ROM:
+    this.loadCHRROM();
+
+    // Do Reset-Interrupt:
+    this.nes.cpu.requestIrq(this.nes.cpu.IRQ_RESET);
+};
+
+
+/**
+ * Mapper007 (AxROM)
+ * @example Battletoads, Time Lord, Marble Madness
+ * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_007
+ * @constructor
+ */
+JSNES.Mappers[7] = function(nes) {
+    this.nes = nes;
+};
+
+JSNES.Mappers[7].prototype = new JSNES.Mappers[0]();
+
+JSNES.Mappers[7].prototype.write = function(address, value) {
+    // Writes to addresses other than MMC registers are handled by NoMapper.
+    if (address < 0x8000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+    } else {
+        this.load32kRomBank(value & 0x7, 0x8000);
+        if(value & 0x10) {
+            this.nes.ppu.setMirroring(this.nes.rom.SINGLESCREEN_MIRRORING2);
+        } else {
+            this.nes.ppu.setMirroring(this.nes.rom.SINGLESCREEN_MIRRORING);
+        }
+    }
+};
+
+JSNES.Mappers[7].prototype.loadROM = function(rom) {
+    if (!this.nes.rom.valid) {
+        alert("AOROM: Invalid ROM! Unable to load.");
+        return;
+    }
+
+    // Load PRG-ROM:
+    this.loadPRGROM();
+
+    // Load CHR-ROM:
+    this.loadCHRROM();
+
+    // Do Reset-Interrupt:
+    this.nes.cpu.requestIrq(this.nes.cpu.IRQ_RESET);
+};
+
+/**
+ * Mapper 011 (Color Dreams)
+ *
+ * @description http://wiki.nesdev.com/w/index.php/Color_Dreams
+ * @example Crystal Mines, Metal Fighter
+ * @constructor
+ */
+JSNES.Mappers[11] = function(nes) {
+    this.nes = nes;
+};
+
+JSNES.Mappers[11].prototype = new JSNES.Mappers[0]();
+
+JSNES.Mappers[11].prototype.write = function(address, value) {
+    if (address < 0x8000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+        return;
+    } else {
+        // Swap in the given PRG-ROM bank:
+        var prgbank1 = ((value & 0xF) * 2) % this.nes.rom.romCount;
+        var prgbank2 = ((value & 0xF) * 2 + 1) % this.nes.rom.romCount;
+
+        this.loadRomBank(prgbank1, 0x8000);
+        this.loadRomBank(prgbank2, 0xC000);
+
+
+        if (this.nes.rom.vromCount > 0) {
+            // Swap in the given VROM bank at 0x0000:
+            var bank = ((value >> 4) * 2) % (this.nes.rom.vromCount);
+            this.loadVromBank(bank, 0x0000);
+            this.loadVromBank(bank + 1, 0x1000);
+        }
+    }
+};
+
+/**
+ * Mapper 034 (BNROM, NINA-01)
+ *
+ * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_034
+ * @example Darkseed, Mashou, Mission Impossible 2
+ * @constructor
+ */
+JSNES.Mappers[34] = function(nes) {
+    this.nes = nes;
+};
+
+JSNES.Mappers[34].prototype = new JSNES.Mappers[0]();
+
+JSNES.Mappers[34].prototype.write = function(address, value) {
+    if (address < 0x8000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+        return;
+    } else {
+        this.load32kRomBank(value, 0x8000);
+    }
+};
+
+
+
+/**
+ * Mapper 066 (GxROM)
+ *
+ * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_066
+ * @example Doraemon, Dragon Power, Gumshoe, Thunder & Lightning,
+ * Super Mario Bros. + Duck Hunt
+ * @constructor
+ */
+JSNES.Mappers[66] = function(nes) {
+    this.nes = nes;
+    console.log('Mapper 66');
+};
+
+JSNES.Mappers[66].prototype = new JSNES.Mappers[0]();
+
+JSNES.Mappers[66].prototype.write = function(address, value) {
+    if (address < 0x8000) {
+        JSNES.Mappers[0].prototype.write.apply(this, arguments);
+        return;
+    } else {
+        // Swap in the given PRG-ROM bank at 0x8000:
+        this.load32kRomBank((value >> 4) & 3, 0x8000);
+
+        // Swap in the given VROM bank at 0x0000:
+        this.load8kVromBank((value & 3) * 2, 0x0000);
+    }
+};
