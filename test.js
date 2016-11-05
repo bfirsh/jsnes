f5b2f005af640aa3ec5fa62e797cc9a69b08ba3b
Bill McKessy <bill@evident.io>
2016-11-05 11:43:06 -0700

    yes

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-
+var jpeg = require('jpeg-js');
 var fs = require('fs')
 
 var nes = require('./exported_source/JSNES.js')({
@@ -38,10 +38,41 @@ var test = []
 for(var i = 0; i < 1000; i++){
   console.log('begin frame')
   nes.frame()
+  nes.keyboard.state1[6] = 0x41
   console.log('end frame')
-  test.push(nes.cpu.mem.slice(0,18))
+
 }
+test.push(nes.ppu.buffer.slice(0,256*240))
 
 test.forEach(function(m){
-  console.log(m.join('\t'))
+  // console.log(m.join('\t'))
+  var x_pos = 0
+  var y_pos = 0
+  var pixel_buffer = new Buffer(m.length*4)
+  var pixel_buffer_idx = 0
+  m.forEach(function(v){
+    // console.log(v)
+    pixel_buffer[pixel_buffer_idx] = (v) & 0xFF
+    pixel_buffer_idx++
+    pixel_buffer[pixel_buffer_idx] = (v >> 8) & 0xFF
+    pixel_buffer_idx++
+    pixel_buffer[pixel_buffer_idx] = (v >> 16) & 0xFF
+    pixel_buffer_idx++
+    pixel_buffer[pixel_buffer_idx] = 255
+    pixel_buffer_idx++
+
+    //     int red = (rgb >> 16) & 0xFF;
+    // int green = (rgb >> 8) & 0xFF;
+    // int blue = rgb & 0xFF;
+  })
+  console.log(pixel_buffer_idx === pixel_buffer.length)
+
+  var r = {
+    data: pixel_buffer,
+    width: 256,
+    height: 240
+  }
+
+  console.log(jpeg.encode(r,100))
+  fs.writeFileSync('./test.jpg', jpeg.encode(r,100).data.toString('binary'), 'binary')
 })
