fd891f3849628d9f680d7ceb06855aa58e83fa30
Ben Firshman <ben@firshman.co.uk>
2017-04-28 15:37:40 +0200

    Run prettier on spec

--- test/nes.spec.js
+++ test/nes.spec.js
@@ -1,45 +1,47 @@
-var assert = require('chai').assert;
-var fs = require('fs');
-var NES = require('../src/nes');
-var sinon = require('sinon');
+var assert = require("chai").assert;
+var fs = require("fs");
+var NES = require("../src/nes");
+var sinon = require("sinon");
 
-describe('NES', function() {
-  it('can be initialized', function() {
+describe("NES", function() {
+  it("can be initialized", function() {
     var nes = new NES();
   });
 
-  it('loads a ROM and runs a frame', function(done) {
+  it("loads a ROM and runs a frame", function(done) {
     var onFrame = sinon.spy();
-    var nes = new NES({onFrame: onFrame});
-    fs.readFile('roms/croom/croom.nes', function(err, data) {
+    var nes = new NES({ onFrame: onFrame });
+    fs.readFile("roms/croom/croom.nes", function(err, data) {
       if (err) return done(err);
-      nes.loadRom(data.toString('ascii'));
+      nes.loadRom(data.toString("ascii"));
       nes.frame();
       assert(onFrame.calledOnce);
       assert.isArray(onFrame.args[0][0]);
-      assert.lengthOf(onFrame.args[0][0], 256*240);
+      assert.lengthOf(onFrame.args[0][0], 256 * 240);
       done();
     });
   });
 
-  describe('#loadRom()', function() {
-    it('throws an error given an invalid ROM', function() {
+  describe("#loadRom()", function() {
+    it("throws an error given an invalid ROM", function() {
       var nes = new NES();
-      assert.throws(function() { nes.loadRom('foo'); }, 'Not a valid NES ROM.');
+      assert.throws(function() {
+        nes.loadRom("foo");
+      }, "Not a valid NES ROM.");
     });
   });
 
-  describe('#getFPS()', function() {
+  describe("#getFPS()", function() {
     var nes = new NES();
     before(function(done) {
-      fs.readFile('roms/croom/croom.nes', function(err, data) {
+      fs.readFile("roms/croom/croom.nes", function(err, data) {
         if (err) return done(err);
-        nes.loadRom(data.toString('ascii'));
+        nes.loadRom(data.toString("ascii"));
         done();
       });
     });
 
-    it('returns an FPS count when frames have been run', function() {
+    it("returns an FPS count when frames have been run", function() {
       assert.isNull(nes.getFPS());
       nes.frame();
       nes.frame();
