b3922c79cfda324bad121211556330c3f4f114e5
allie <littlestcake@gmail.com>
2018-10-27 21:30:41 -0400

    Add support for mapper 180

--- src/mappers.js
+++ src/mappers.js
@@ -1381,4 +1381,45 @@ Mappers[66].prototype.write = function(address, value) {
   }
 };
 
+/**
+ * Mapper 180
+ *
+ * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_180
+ * @example Crazy Climber
+ * @constructor
+ */
+Mappers[180] = function(nes) {
+  this.nes = nes;
+};
+
+Mappers[180].prototype = new Mappers[0]();
+
+Mappers[180].prototype.write = function(address, value) {
+  // Writes to addresses other than MMC registers are handled by NoMapper.
+  if (address < 0x8000) {
+    Mappers[0].prototype.write.apply(this, arguments);
+    return;
+  } else {
+    // This is a ROM bank select command.
+    // Swap in the given ROM bank at 0xc000:
+    this.loadRomBank(value, 0xc000);
+  }
+};
+
+Mappers[180].prototype.loadROM = function() {
+  if (!this.nes.rom.valid) {
+    throw new Error("Mapper 180: Invalid ROM! Unable to load.");
+  }
+
+  // Load PRG-ROM:
+  this.loadRomBank(0, 0x8000);
+  this.loadRomBank(this.nes.rom.romCount - 1, 0xc000);
+
+  // Load CHR-ROM:
+  this.loadCHRROM();
+
+  // Do Reset-Interrupt:
+  this.nes.cpu.requestIrq(this.nes.cpu.IRQ_RESET);
+};
+
 module.exports = Mappers;
