976b02fd6be0fa49fc0604015e0716fdd7ff8fab
allie <littlestcake@gmail.com>
2018-10-27 22:42:39 -0400

    Add support for mapper 38

--- src/mappers.js
+++ src/mappers.js
@@ -1355,6 +1355,32 @@ Mappers[34].prototype.write = function(address, value) {
 };
 
 /**
+ * Mapper 038
+ *
+ * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_038
+ * @example Crime Busters
+ * @constructor
+ */
+Mappers[38] = function(nes) {
+  this.nes = nes;
+};
+
+Mappers[38].prototype = new Mappers[0]();
+
+Mappers[38].prototype.write = function(address, value) {
+  if (address < 0x7000 || address > 0x7fff) {
+    Mappers[0].prototype.write.apply(this, arguments);
+    return;
+  } else {
+    // Swap in the given PRG-ROM bank at 0x8000:
+    this.load32kRomBank(value & 3, 0x8000);
+
+    // Swap in the given VROM bank at 0x0000:
+    this.load8kVromBank(((value >> 2) & 3) * 2, 0x0000);
+  }
+};
+
+/**
  * Mapper 066 (GxROM)
  *
  * @description http://wiki.nesdev.com/w/index.php/INES_Mapper_066
